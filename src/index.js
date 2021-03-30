1efe769ef61c9d69e245a73ee0f1dd7f3fbb599d
alvinashiatey <alvinashiatey@gmail.com>
2021-03-29 23:05:18 -0400

    netlify

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
-console.log('Hello World!');
+import "../sass/main.scss";
+
+let xs = [];
+let screenWidth = window.innerWidth;
+
+for (var i = 0; i <= screenWidth; i++) {
+  xs.push(i);
+}
+
+let t = 0;
+
+function animate() {
+  let points = xs.map((x) => {
+    let y = 25 + 5 * Math.sin((x + t) / 5);
+
+    return [x, y];
+  });
+
+  let path =
+    "M" +
+    points
+      .map((p) => {
+        return p[0] + "," + p[1];
+      })
+      .join(" L");
+
+  document.querySelector("path").setAttribute("d", path);
+
+  t += 0.5;
+
+  requestAnimationFrame(animate);
+}
+
+animate();
--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,53 @@
 // Generated using webpack-cli http://github.com/webpack-cli
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-    },
-    devServer: {
-        open: true,
-        host: 'localhost',
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'index.html',
-        }),
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    open: true,
+    host: "localhost",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "index.html",
+    }),
 
-        // Add your plugins here
-        // Learn more obout plugins from https://webpack.js.org/configuration/plugins/
-    ],
-    module: {
-        rules: [
-            {
-                test: /\\.(js|jsx)$/,
-                loader: 'babel-loader',
+    // Add your plugins here
+    // Learn more obout plugins from https://webpack.js.org/configuration/plugins/
+  ],
+  module: {
+    rules: [
+      {
+        test: /\\.(js|jsx)$/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          {
+            loader: "style-loader",
+            options: {
+              insert: "head", // insert style tag inside of <head>
+              injectType: "singletonStyleTag", // this is for wrap all your style in just one style tag
             },
-            {
-                test: /\.s[ac]ss$/i,
-                use: ['style-loader', 'css-loader', 'sass-loader'],
-            },
-            {
-                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/,
-                type: 'asset',
-            },
-
-            // Add your rules for custom modules here
-            // Learn more about loaders from https://webpack.js.org/loaders/
+          },
+          "css-loader",
+          "resolve-url-loader",
+          "sass-loader",
         ],
-    },
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/,
+        type: "asset",
+      },
+
+      // Add your rules for custom modules here
+      // Learn more about loaders from https://webpack.js.org/loaders/
+    ],
+  },
 };
