a9b381b1822a82215d6938debfcb84df55d0f27b
alvinashiatey <alvinashiatey@gmail.com>
2021-03-30 10:17:30 -0400

    update

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import "../sass/main.scss";
+import * as Tone from "tone";
 
 let xs = [];
 let screenWidth = window.innerWidth;
@@ -32,3 +33,30 @@ function animate() {
 }
 
 animate();
+
+//sound
+// create two monophonic synths
+const trigger = document.querySelector("#wave");
+const synthA = new Tone.FMSynth().toDestination();
+const synthB = new Tone.AMSynth().toDestination();
+let playing = false;
+trigger.addEventListener("click", () => {
+  if (playing === false) {
+    //play a note every quarter-note
+    const loopA = new Tone.Loop((time) => {
+      synthA.triggerAttackRelease("C2", "8n", time);
+    }, "4n").start(0);
+    //play another note every off quarter-note, by starting it "8n"
+    const loopB = new Tone.Loop((time) => {
+      synthB.triggerAttackRelease("C4", "16n", time);
+    }, "4n").start("8n");
+    // the loops start when the Transport is started
+    Tone.Transport.start();
+    // ramp up to 800 bpm over 10 seconds
+    Tone.Transport.bpm.rampTo(130, 10);
+    setTimeout(() => (playing = true), 2000);
+  } else {
+    Tone.Transport.stop();
+    playing = false;
+  }
+});
